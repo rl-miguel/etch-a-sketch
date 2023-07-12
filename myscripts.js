5fb901e0bb686c80d27cca1386e62202a1a6e62e
rl-miguel <ruelmiguel@protonmail.com>
2023-07-12 23:18:59 +0100

    Add divs through DOM manipulation

---
+++ myscripts.js
@@ -0,0 +1,38 @@
+const body = document.querySelector('body');
+let divContainer = document.createElement('div');
+
+let div1= document.createElement('div');
+let div2= document.createElement('div');
+let div3= document.createElement('div');
+let div4= document.createElement('div');
+let div5= document.createElement('div');
+let div6= document.createElement('div');
+let div7= document.createElement('div');
+let div8= document.createElement('div');
+let div9= document.createElement('div');
+let div10= document.createElement('div');
+let div11= document.createElement('div');
+let div12= document.createElement('div');
+let div13= document.createElement('div');
+let div14= document.createElement('div');
+let div15= document.createElement('div');
+let div16= document.createElement('div');
+
+body.appendChild(divContainer);
+divContainer.className='divContainer';
+divContainer.appendChild(div1);
+divContainer.appendChild(div2);
+divContainer.appendChild(div3);
+divContainer.appendChild(div4);
+divContainer.appendChild(div5);
+divContainer.appendChild(div6);
+divContainer.appendChild(div7);
+divContainer.appendChild(div8);
+divContainer.appendChild(div9);
+divContainer.appendChild(div10);
+divContainer.appendChild(div11);
+divContainer.appendChild(div12);
+divContainer.appendChild(div13);
+divContainer.appendChild(div14);
+divContainer.appendChild(div15);
+divContainer.appendChild(div16);
