fe7ae4c9400dcd2588aa732d4dee34bf16446acc
rl-miguel <ruelmiguel@protonmail.com>
2023-07-13 16:01:34 +0100

    Randomize hexcode value for background color

--- myscripts.js
+++ myscripts.js
@@ -4,22 +4,19 @@ const divContainer=document.getElementById('divContainer');
 
 function createGrid(){
 for(i=0;i<256;i++){
-let div = document.createElement('div');
-divContainer.appendChild(div);
-div.classList.add('etchGrid');
-div.addEventListener('mouseover', colorMyDiv);
+    let div = document.createElement('div');
+    divContainer.appendChild(div);
+    div.classList.add('etchGrid');
+    div.addEventListener('mouseover', colorMyDiv);
 
+let randomColor=0;
 function colorMyDiv(){
-    div.classList.add('gridMouseOver')
+    randomColor=Math.floor(Math.random()*16777215).toString(16);
+    div.style.backgroundColor=`#${randomColor}`;
+    
 }
 }};
 
 createGrid()
 
-function countDiv(){
-    let numberOfDivs = document.getElementsByClassName('etchGrid');
-    console.log(numberOfDivs);
-}
-
-countDiv()
 
