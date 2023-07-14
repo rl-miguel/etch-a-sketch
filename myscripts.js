dfc25e770c63ec0b72c5282173b54e408b61434a
rl-miguel <ruelmiguel@protonmail.com>
2023-07-14 19:58:29 +0100

    Add reset grid functionality for button

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,6 @@
 const body=document.querySelector('body');
 const divContainer=document.getElementById('divContainer');
-
+const btnReset = document.getElementsByClassName('btn-reset');
 
 function createGrid(){
 for(i=0;i<256;i++){
@@ -13,10 +13,26 @@ let randomColor=0;
 function colorMyDiv(){
     randomColor=Math.floor(Math.random()*16777215).toString(16);
     div.style.backgroundColor=`#${randomColor}`;
-    
 }
-}};
+
+    function resetEtchGrids(){
+        div.style.backgroundColor='#6C737E';
+    }
+
+btnReset[0].addEventListener('click', resetEtchGrids);
+}
+};
 
 createGrid()
+
+
+
+
+
+
+
+
+
+
 
 
